a026931522fe314838e905eb929a1917cec1334f
Anwesha Mishra <anwesharoses@gmail.com>
2022-01-17 11:12:53 +0800

    Active users chart done

---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const userData = [
+    {
+      name: 'Jan',
+      users: 4000,
+    },
+    {
+      name: 'Feb',
+      users: 3000,
+    },
+    {
+      name: 'March',
+      users: 5000,
+    },
+    {
+      name: 'April',
+      users: 4000,
+    },
+    {
+      name: 'May',
+      users: 3000,
+    },
+    {
+      name: 'June',
+      users: 2000,
+    },
+    {
+      name: 'July',
+      users: 4000,
+    },
+    {
+      name: 'Aug',
+      users: 3000,
+    },
+    {
+      name: 'Sept',
+      users: 4000,
+    },
+    {
+      name: 'Oct',
+      users: 1000,
+    },
+    {
+      name: 'Nov',
+      users: 4000,
+    },
+    {
+      name: 'Dec',
+      users: 3000,
+    },
+
+  ];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 
+import Chart from "../../components/charts/Chart"
 import Featured from "../../components/featured/Featured"
 import "./home.css"
+import { userData } from "../../dummyData"
 
 export default function Home() {
     return (
         <div className="home">
             <Featured/>
+            <Chart data={userData} title="User Analytics" grid dataKey="users"/>
         </div>
     )
 }
